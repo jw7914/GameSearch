d12743108240941a21c10d0a21deb1c3aa35fb56
jw7914 <jw7914@nyu.edu>
2024-09-03 17:58:26 -0400

    sample / test backend flask server

--- server/main.py
+++ server/main.py
@@ -1,14 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import os
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def users():
-    return jsonify(
-        {
-            "test": ['Hello', "World"]
-        }
-    )
+client_id = os.getenv('CLIENT_ID')
+access_token = os.getenv('ACCESS_TOKEN')
+base_url = "https://api.igdb.com/v4"
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+def fetch_games(search_term):
+    headers = {
+        'Client-ID': client_id,
+        'Authorization': f'Bearer {access_token}',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    body = f'fields id, name, cover; search "{search_term}";'
+    
+    response = requests.post(f'{base_url}/games', headers=headers, data=body)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+@app.route('/games', methods=['GET'])
+def get_games():
+    search_term = request.args.get('search_term', default='zelda', type=str)
+    try:
+        games = fetch_games(search_term)
+        return jsonify(games)
+    except requests.exceptions.HTTPError as err:
+        return jsonify({"error": str(err)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
