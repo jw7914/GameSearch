693e18649f4967982f108a70f3ec223626ac7bdb
jw7914 <jw7914@nyu.edu>
2024-09-04 16:57:34 -0400

    general skeleton for a search result page / testing routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './components/pages/Home'
+import Result from './components/pages/Result'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </BrowserRouter>     
 
   )
