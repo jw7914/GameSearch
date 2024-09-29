9b7ee2620f2e8d107af51a40197ef9a58c3ff957
Will0714160 <patrickyan51418@gmail.com>
2024-09-28 23:28:03 -0400

    ClMade the code more efficient

--- server/main.py
+++ server/main.py
@@ -64,11 +64,13 @@ def get_parent_game_detail(gameName):
 def create_list_of_games(games):
     games_with_all_fields = []
     uniqueGames = []
+    uniqueGameNames = set()
     for game in games:
         if game.get("parent_game", "") == "":
             uniqueGames.append(game)
-        elif game.get("parent_game", "") != "" and game["parent_game"] not in uniqueGames:
+        elif game.get("parent_game", "") != "" and game['parent_game']['name'] in uniqueGameNames:
             uniqueGames.append(get_parent_game_detail((game['parent_game']['name'])))
+            uniqueGameNames.add(game['parent_game']['name'])
     
     required_keys = ["name", "cover", "summary", "genres", "screenshots"]
     
