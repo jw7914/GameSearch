fe2202975caea4dcf552a615c57c39afff31eac3
jw7914 <jw7914@nyu.edu>
2024-09-30 14:45:53 -0400

    front-end testing for vercel

--- client/src/components/pages/SearchResults.jsx
+++ client/src/components/pages/SearchResults.jsx
@@ -8,6 +8,7 @@ import "./SearchResults.css";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import GameCard from "../Gamecard";
+
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
