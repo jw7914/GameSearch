f5c10a411134aadc477499dfd4eb763ed2bef807
Jason Wu <124104246+jw7914@users.noreply.github.com>
2026-08-02 07:46:04 -0400

    Update firebaseConfig.jsx to clean up comments

--- client/firebase/firebaseConfig.jsx
+++ client/firebase/firebaseConfig.jsx
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API,
   authDomain: import.meta.env.VITE_AUTH,
@@ -14,17 +10,10 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const firebaseapp = initializeApp(firebaseConfig);
-
-// Initialize authentication
-const auth = getAuth(firebaseapp);
-
-// If you're using analytics, initialize it like this:
-// const analytics = getAnalytics(firebaseapp);
-
-// Export as named exports
-export { firebaseapp, auth };
+export const firebaseapp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseapp);
+export const db = getFirestore(firebaseapp);
